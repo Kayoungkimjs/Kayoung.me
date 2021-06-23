bc245db7989d8ca22e4307a764230d2fb5294457
Kayoungkimjs <83155239+Kayoungkimjs@users.noreply.github.com>
2021-06-23 13:03:04 +0900

    Add JavaScript codes

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 "use strict";
+const btn1 = document.querySelector(".career-card-1")
+const btn2 = document.querySelector(".career-card-2")
+const btn3 = document.querySelector(".career-card-3")
+const card1 = document.querySelector(".description-1")
+const card2 = document.querySelector(".description-2")
+const card3 = document.querySelector(".description-3")
+
+
+
+btn1.addEventListener("click", function() {
+    card1.classList.toggle("show");
+})
+
+btn2.addEventListener("click", function() {
+    card2.classList.toggle("show");
+})
+
+btn3.addEventListener("click", function() {
+    card3.classList.toggle("show");
+})
